14cafb898b4330e669d7ba5b840641b73ed0f853
t210mha <t210mha@gmail.com>
2024-06-30 18:40:02 +0900

    faq作成

--- assets/script/main.js
+++ assets/script/main.js
@@ -21,3 +21,48 @@ $(window).on('resize', function () {
     // ハンバーガーメニューを閉じる
     $('.header__nav').removeClass('header__nav--active');
 });
+
+/*-----------------------------------------
+    Topページ
+-----------------------------------------*/
+
+// ---------- アコーディオン ----------
+$('.faq__item-answer').hide(); // 初期表示を非表示に
+$('.faq__item-question').on('click', function (e) {
+
+    var content = $(e.target);
+
+    if (content.hasClass('faq__item-question')) {
+        content = content.next();
+    }
+    else if (content.hasClass('question__badges')) {
+        content = content.parent().parent().next();
+    }
+    else if (content.hasClass('question__badge')) {
+        content = content.parent().parent().parent().next();
+    }
+    else if (content.hasClass('question__badge-text')) {
+        content = content.parent().parent().parent().parent().next();
+    }
+    else if (content.hasClass('question__text')) {
+        content = content.parent().parent().next();
+    }
+    else if (content.hasClass('question__button-image')) {
+        content = content.parent().parent().next();
+    }
+    else {
+        // question__mark, question__content, question__button
+        content = content.parent().next();
+    }
+    content.slideToggle();
+
+    var btn_img = $('.faq__item-question').find('img');
+    if ($(this).hasClass('open')) {
+        $(this).removeClass('open');
+        btn_img.attr('src', '/../assets/image/faq/faq/icon_plus_01.png');
+    }
+    else {
+        $(this).addClass('open')
+        btn_img.attr('src', '/../assets/image/faq/faq/icon_minus_01.png');
+    }
+});
